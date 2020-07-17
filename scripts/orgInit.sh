f606cac61ee5ca8568096382906e45bcbee63643
Tim Perdue <tperdue@salesforce.com>
2020-07-16 23:01:05 -0400

    update script

--- scripts/orgInit.sh
+++ scripts/orgInit.sh
@@ -6,8 +6,8 @@ if [ "$#" -eq 1 ]; then
   DURATION=$1
 fi
 
-sfdx force:org:create -a dreamhouse -s -f config/project-scratch-def.json -d $DURATION
-sfdx force:source:push
-sfdx force:user:permset:assign -n dreamhouse
+sfdx force:org:create -a ProjectScratch -s -f config/project-scratch-def.json -d $DURATION
+sfdx force:source:push -f -u ProjectScratch
+sfdx force:user:permset:assign -n Projects
 sfdx force:org:open -p /lightning/page/home
 echo "Org is set up"
