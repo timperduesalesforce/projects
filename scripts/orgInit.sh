6a047888a7e52128d2438d10af66ace0e1fc8702
Tim Perdue <tperdue@salesforce.com>
2020-07-19 12:28:05 -0400

    update script again

--- scripts/orgInit.sh
+++ scripts/orgInit.sh
@@ -10,4 +10,4 @@ sfdx force:org:create -a ProjectScratch -s -f config/project-scratch-def.json -d
 sfdx force:source:push -f -u ProjectScratch
 sfdx force:user:permset:assign -n Projects
 sfdx force:org:open -p /lightning/page/home
-echo "This org is setup"
+echo "This org has been setup"
